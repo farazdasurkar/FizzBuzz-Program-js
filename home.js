319e0003836eb83038f572f0c3a09f1d71ed2048
farazdasurkar <farazdasurkar@users.noreply.github.com>
2021-10-31 23:25:54 +0530

    commit

--- home.js
+++ home.js
@@ -2,7 +2,16 @@ var output = [];
 var count = 1;
 
 function fizzBuzz() {
-    output.push(count);
+    if (count % 3 === 0) {
+        output.push("Fizz");
+    }
+    else if (count % 5 === 0) {
+        output.push("Buzz");
+    }
+    else {
+        output.push(count);
+    }
+
     console.log(output);
     count++;
 
